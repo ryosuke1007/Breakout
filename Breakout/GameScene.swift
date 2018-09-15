151b7b757f907517b26209a3c7ba7c2b5b83529b
木村　僚介 <mk0212.ryosuke1007@gmail.com>
2018-09-15 16:16:49 +0900

    Hello

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,81 +9,187 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene,SKPhysicsContactDelegate {
+    var ball:SKSpriteNode!
+    var paddle: SKSpriteNode!
+    var time: Int = 0
+    var count: Int = 0
+    var isGameover: Bool = false
+    var score: Int = 0
+    var startDate = NSDate()
+    var bricks: [SKNode] = []
+    var scorelabel: SKLabelNode!
+    var gameoverlabel: SKLabelNode!
+    var messagelabel: SKLabelNode!
+    var gamescorelabel: SKLabelNode!
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    
     
     override func didMove(to view: SKView) {
+        super.didMove(to: view)
+        Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(GameScene.timerUpdate), userInfo: nil, repeats: true)
+        ball = self.childNode(withName: "Ball") as! SKSpriteNode
+        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+        paddle = self.childNode(withName: "Paddle") as! SKSpriteNode
+        scorelabel = self.childNode(withName: "Label") as! SKLabelNode
+        gameoverlabel = self.childNode(withName: "gameoverlabel") as! SKLabelNode
+        messagelabel = self.childNode(withName: "messagelabel") as! SKLabelNode
+        gamescorelabel = self.childNode(withName: "gamescorelabel") as! SKLabelNode
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)
+        border.friction = 0
+        self.physicsBody = border
+        
+        self.physicsWorld.contactDelegate = self //ボールの衝突時の判定をここでする
+        
+        for child in children {
+            if child.name == "Brick" {
+                bricks.append(child)
+            }
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        scorelabel.text = String(score)
     }
     
     
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    @objc func timerUpdate() {
+        var newvelocity = (ball.physicsBody?.velocity)!
+        if newvelocity.dx <= 800{
+            newvelocity.dx *= 1.0001
         }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+        if newvelocity.dy <= 800{
+            newvelocity.dy *= 1.0001
         }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        else if isGameover == true {
+            newvelocity.dx *= 0
+            newvelocity.dy *= 0
         }
+        if newvelocity.dx == 0 {
+            if newvelocity.dy == 0{
+                ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
+                print("1")
+            }
+        }
+        ball.physicsBody?.velocity = newvelocity
+        time = time + 1
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
+            
+            if isGameover == true {
+                ball.position.x = touchLocation.x
+                ball.position.y = 0
+                self.addChild(ball)
+                score = 0
+                scorelabel.text = String(score)
+                gameoverlabel.text = ""
+                messagelabel.text = ""
+                gamescorelabel.text = ""
+                    for Brick in bricks {
+                        self.addChild(Brick)
+                    }
+                isGameover = false
+                
+                
+            }
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for touch in touches {
+            let touchLocation = touch.location(in: self)
+            paddle.position.x = touchLocation.x
+        }
     }
     
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    
+    func didBegin (_ contact: SKPhysicsContact) {
+        let bodyAName = contact.bodyA.node?.name
+        let bodyBName = contact.bodyB.node?.name
+        
+        if bodyAName == "Wall" && bodyBName == "Ball" || bodyAName == "Ball" && bodyBName == "Wall"{
+            if bodyAName == "Ball" {
+                contact.bodyA.node?.removeFromParent()
+                let particle = SKEmitterNode(fileNamed: "spark.sks" )
+                particle!.position = CGPoint(x:contact.contactPoint.x, y:contact.contactPoint.y)
+                let action1 = SKAction.wait(forDuration: 0.5)
+                let action2 = SKAction.removeFromParent()
+                let actionAll = SKAction.sequence([action1, action2])
+                self.addChild(particle!)
+                particle!.run(actionAll)
+                gameoverlabel.text = "GAMEOVER"
+                messagelabel.text = "Your score is"
+                gamescorelabel.text = String(score)
+                count = 0
+                for Brick in bricks {
+                    Brick.removeFromParent()
+                }
+                isGameover = true
+                
+            } else if bodyBName == "Ball" {
+                contact.bodyB.node?.removeFromParent()
+                let particle = SKEmitterNode(fileNamed: "spark.sks" )
+                particle!.position = CGPoint(x:contact.contactPoint.x, y:contact.contactPoint.y)
+                let action1 = SKAction.wait(forDuration: 0.5)
+                let action2 = SKAction.removeFromParent()
+                let actionAll = SKAction.sequence([action1, action2])
+                self.addChild(particle!)
+                particle!.run(actionAll)
+                gameoverlabel.text = "GAMEOVER"
+                messagelabel.text = "Your score is"
+                gamescorelabel.text = String(score)
+                count = 0
+                for Brick in bricks {
+                    Brick.removeFromParent()
+                }
+                isGameover = true
+                
+            }
+        }
+        
+        
+        if bodyAName == "Ball" && bodyBName == "Brick" || bodyAName == "Brick" && bodyBName == "Ball"{
+            if bodyAName == "Brick" {
+                contact.bodyA.node?.removeFromParent()
+                count = count + 1
+                score = score + 50
+                scorelabel.text = String(score)
+                let particle = SKEmitterNode(fileNamed: "spark.sks" )
+                particle!.position = (contact.bodyA.node?.position)!;
+                let action1 = SKAction.wait(forDuration: 0.3)
+                let action2 = SKAction.removeFromParent()
+                let actionAll = SKAction.sequence([action1, action2])
+                self.addChild(particle!)
+                particle!.run(actionAll)
+            } else if bodyBName == "Brick" {
+                contact.bodyB.node?.removeFromParent()
+                count = count + 1
+                score = score + 50
+                scorelabel.text = String(score)
+                let particle = SKEmitterNode(fileNamed: "spark.sks" )
+                particle!.position = (contact.bodyB.node?.position)!;
+                let action1 = SKAction.wait(forDuration: 0.3)
+                let action2 = SKAction.removeFromParent()
+                let actionAll = SKAction.sequence([action1, action2])
+                self.addChild(particle!)
+                particle!.run(actionAll)
+            }
+        }
+        
+        if bodyAName == "Paddle" && bodyBName == "Ball" || bodyAName == "Ball" && bodyBName == "Paddle" {
+            if count >= 9 {
+                for Brick in bricks {
+                    self.addChild(Brick)
+                    count = 0
+                }
+            }
+        }
     }
 }
+
---
+++ Breakout/TitleScene.swift
@@ -0,0 +1,10 @@
+//
+//  TitleScene.swift
+//  Breakout
+//
+//  Created by 木村　僚介 on 2018/09/15.
+//  Copyright © 2018年 木村　僚介. All rights reserved.
+//
+
+import SpriteKit
+
